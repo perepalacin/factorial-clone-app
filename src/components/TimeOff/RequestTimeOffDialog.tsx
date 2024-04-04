895d727c5add5f23469001e64915f4e075993ec5
perepalacin <perepalacin@gmail.com>
2024-04-05 00:28:13 +0200

    mvp frontend complete

--- src/components/TimeOff/RequestTimeOffDialog.tsx
+++ src/components/TimeOff/RequestTimeOffDialog.tsx
@@ -1,30 +1,49 @@
-import { PalmtreeIcon, PlusIcon } from 'lucide-react'
-import { useState } from 'react'
+import { PalmtreeIcon, PlusIcon, XIcon } from 'lucide-react'
+import { useEffect, useState } from 'react'
 import '../../index.css'
 import SelectDateRange from './SelectRange';
 
 const RequestTimeOffDialog = () => {
 
     const [dialogVisible, setIsVisible] = useState(false);
+
+    const handlePressEsc = (event: KeyboardEvent) => {
+        if (event.key === "Escape"  || event.code === '0x0001') {
+            setIsVisible(false);
+        }
+    }
+
+    useEffect(() => {
+        document.addEventListener('keydown', handlePressEsc, true);
+        return () => {
+            document.removeEventListener('keydown', handlePressEsc, true);
+        };
+    }, []);
+
   return (
     <div>
     <button onClick = {() => setIsVisible((prevState) => {return !prevState})}className="main-button big-button" style={{display: 'flex', flexDirection: 'row', gap: '0.5rem', alignItems: 'center'}}>
         <PlusIcon className="icon" />
-        <p>Request time off</p>
+        <p style={{fontWeight: '700'}}>Request time off</p>
     </button>  
     {
         dialogVisible 
         ?
-        <div style={{position: 'absolute', top: 0, left: 0, display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100%', backgroundColor: 'rgba(0,0,0,0.2)', zIndex: '20'}}>
+        <div style={{position: 'absolute', top: 0, left: 0, display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', width: '100%', zIndex: '20', height: '100%'}}>
+            <div style={{position: 'absolute', top: 0, left: 0, display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', width: '100%', backgroundColor: 'rgba(0,0,0,0.2)', height:'115%',flexGrow: 1}} onClick={() => {setIsVisible(false)}}>
+            </div>
             <div style={{maxWidth: '250px', minWidth: '250px', display:'flex', flexDirection: 'column', gap: '0.5rem', position:'relative', backgroundColor: '#FFFFFF', padding: '2rem 1rem', borderRadius: '1rem', boxShadow: '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)'}}>
+                <button style={{backgroundColor: '#FFFFFF', color: '#AEAEB8' ,border: 'none', position: 'absolute', top: '1rem', right: '0.5rem'}} onClick={() => {setIsVisible(false)}}>
+                    <XIcon size={18}/>
+                </button>
                 <div style={{position: 'absolute', marginTop: '-60px', left: '2', padding: '0.5rem 0.67rem', backgroundColor: '#FFFFFF', color: '#FF355E', borderRadius: '0.5rem', boxShadow: '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)'}}>
                     <PalmtreeIcon size={30}/>
                 </div>
                 <p style={{fontWeight: '600'}}>Resquest time off</p>
-                <p style={{fontSize: '0.8rem'}}>We will  notfiy all of those who have permission to approve your time off about this request, including the person responsible for supervising them.</p>
-                <label htmlFor='select' style={{fontWeight: '600', fontSize: '0.8rem'}} >Type of absence</label>
-                <select className="">
-                    <option value="PTO">Holidays - Discounts time and needs approval</option>
+                <p style={{fontSize: '0.8rem', textAlign: 'justify'}}>We will  notfiy all of those who have permission to approve your time off about this request, including the person responsible for supervising them.</p>
+                <label htmlFor='select' style={{fontWeight: '600', fontSize: '0.9rem'}} >Type of absence</label>
+                <select className='dropdown-btn' autoFocus>
+                    <option className = 'dropdown-option' value="PTO">Holidays - Discounts time and needs approval</option>
                     <option value="NPTO">No paid leave - Dost not discount time and needs approval</option>
                     <option value="Compensation">Overtime compensation - Dost not discount time and needs approval</option>
                     <option value="medical">Medical absence - Dost not discount time and needs approval</option>
@@ -32,8 +51,10 @@ const RequestTimeOffDialog = () => {
                     <option value="Otherst">Paid leave: Other reasons - Does not discount time and needs approval</option>
                 </select>
                 <label style={{fontWeight: '600', fontSize: '0.8rem'}}  >Description</label>
-                <textarea style={{maxWidth: '100%', minWidth: '100%', maxHeight: '6rem', fontFamily: 'Inter', color: 'black', fontWeight: '400'}} rows={3}/>
-                <SelectDateRange />
+                <textarea style={{maxWidth: '14.5rem', maxHeight: '7rem', fontFamily: 'Inter', color: 'black', fontWeight: '400', padding: '0.5rem'}} rows={3}/>
+                <div style={{zIndex: '50'}}>
+                    <SelectDateRange />
+                </div>
             </div>
         </div>
     :
--- src/components/TimeOff/SelectRange.tsx
+++ src/components/TimeOff/SelectRange.tsx
@@ -2,20 +2,31 @@ import { useState } from "react";
 
 import { addDays, format } from "date-fns";
 import { DateRange, DayPicker } from "react-day-picker";
+import { shortFormatter } from "../../utils/dateTools";
 
 const today = new Date();
 
 const css = `
-  .my-selected:not([disabled]) { 
+  .rdp-day_selected { 
     position: relative;
-    color: #000000;
+    color: #FFFFFF;
     background-color: #FF355E;
+    border-radius: 0.375rem;
   }
   .my-selected:not:last-child([disabled]) { 
     background-color: #FF0000;
   }
-  .my-today {
-    border: 2px solid #0BA3AE; 
+  .rdp-day_today:not(.rdp-day_outside)  {
+    font-weight: 500;
+  }
+  .rdp:not([dir='rtl']) .rdp-day_range_start:not(.rdp-day_range_end) {
+    border-radius: 0.375rem;
+  }
+  .rdp:not([dir='rtl']) .rdp-day_range_end:not(.rdp-day_range_start) {
+    border-radius: 0.375rem;
+  }
+  .rdp-day_range_end.rdp-day_range_start {
+    border-radius: 0.365rem;
   }
 `;
 
@@ -52,9 +63,20 @@ export default function SelectDateRange() {
           mode="range"
           defaultMonth={today}
           selected={range}
-          footer={footer}
           onSelect={setRange}
           />
+          <label htmlFor='select' style={{fontWeight: '600', fontSize: '0.9rem'}} >Selected days</label>
+          <p style={{fontSize: '0.8rem', padding: '0.5rem', border: 'solid 2px #E2E2E5', borderRadius: '0.5rem'}}>{range?.from?.getDate()} {shortFormatter.format(range?.from?.getMonth())} {range?.from?.getFullYear()} - {range?.to?.getDate()} {shortFormatter.format(range?.to?.getMonth())} {range?.to?.getFullYear()}</p>
+          {range?.from && range.to 
+          ?
+          <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'end', marginTop: '1rem'}}>
+            <button className="small-button main-button">
+              Request days off
+            </button>
+          </div>
+          :
+          <></>  
+          }
           </div>
       ) : (
         <></>
